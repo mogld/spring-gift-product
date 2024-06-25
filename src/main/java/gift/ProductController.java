25e017b0b14fda9c2ac32f9c9e12f637a5694552
mogld <143364802+mogld@users.noreply.github.com>
2024-06-25 16:06:48 +0900

    feat: 상품추가 기능

---
+++ src/main/java/gift/ProductController.java
@@ -0,0 +1,29 @@
+package gift;
+
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/products")
+public class ProductController {
+    private final Map<Long, Product> products = new HashMap<>();
+    private long currentId = 0;
+
+    @PostMapping
+    public ResponseEntity<Product> addProduct(@RequestBody Product product) {
+        currentId++;
+        product.setId(currentId);
+        products.put(currentId, product);
+        return new ResponseEntity<>(product, HttpStatus.CREATED);
+    }
+
+
+}
+
